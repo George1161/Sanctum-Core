4e4c35fffaef2e58c9ac85154992c8b2274ef28e
George1161 <135200161+George1161@users.noreply.github.com>
2025-04-10 08:31:09 -0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center text-center text-white bg-black">
-      <h1 className="text-5xl font-bold">Welcome to Sanctum Core</h1>
-    </main>
-  );
-}
